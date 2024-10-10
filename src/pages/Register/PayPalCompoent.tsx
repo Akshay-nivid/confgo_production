c1b837af99d3c569d61c393691199220895e9b44
AthulPP <ctr.athul.p@prth.com>
2024-10-10 10:27:58 +0530

    .

--- src/pages/Register/PayPalCompoent.tsx
+++ src/pages/Register/PayPalCompoent.tsx
@@ -1,47 +1,3 @@
-// import React from 'react';
-// import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
-// import Grid from '@mui/material/Grid2';
-// /*
-//  * Component used to handle paypal button 
-//  */
-// const PayPalButton: React.FC = () => {
-//     const initialOptions = {
-//         clientId: "AQ9K1hDjjXSmmQz1aBt3FDjLTkrl8DRJvnUC6H6_eXAw-wzz6eC2eoYmSOEJcdN0prPUX1hsSm8bfGtK", // Replace with your PayPal Client ID
-//         currency: "USD",
-//         intent: "capture",
-//     };
-// /*
-//  * functional compoent used to approve the payment functionality
-// *@ param
-//  */
-//     const handleApprove = async (data: any, actions: any) => {
-//         const details = await actions.order.capture();
-//         console.log('Transaction completed by ' + details.payer.name.given);
-//         alert('Transaction completed by ' + details.payer.name.given);
-//     };
-
-//     return (
-//         <Grid >
-//         <PayPalScriptProvider options={initialOptions}>
-//             <PayPalButtons 
-//                 style={{ layout: 'vertical', }}
-//                 createOrder={(data, actions) => {
-//                     return actions.order.create({
-//                         purchase_units: [{
-//                             amount: {
-//                                 currency_code: 'USD',
-//                                 value: '10.00', 
-//                             },
-//                         }],
-//                         intent: 'CAPTURE'
-//                     });
-//                 }}
-//                 onApprove={handleApprove}
-//             />
-//         </PayPalScriptProvider>
-//         </Grid>
-//     );
-// };
 import React, { useRef } from 'react';
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 import Grid from '@mui/material/Grid2';
@@ -62,7 +18,7 @@ const PayPalButton: React.FC = () => {
      * Functional component used to approve the payment functionality
      * @param
      */
-    const handleApprove = async (data: any, actions: any) => {
+    const handleApprove = async (_data: any, actions: any) => {
         const details = await actions.order.capture();
         console.log('Transaction completed by ' + details.payer.name.given);
         alert('Transaction completed by ' + details.payer.name.given);
@@ -80,7 +36,7 @@ const PayPalButton: React.FC = () => {
                 <div ref={paypalButtonRef}>
                     <PayPalButtons
                         style={{ layout: 'vertical' }}
-                        createOrder={(data, actions) => {
+                        createOrder={(_data, actions) => {
                             return actions.order.create({
                                 purchase_units: [{
                                     amount: {
