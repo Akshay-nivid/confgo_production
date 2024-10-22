b1fbbaccadd6a4327442b2b5c65300daaaac1f8a
Neethu <ctr.neethu.kp@prth.com>
2024-10-22 23:02:54 +0530

    COUPON list add and edit

---
+++ src/pages/coupon/CouponView.tsx
@@ -0,0 +1,306 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, Snackbar, Alert, CircularProgress } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { useNavigate, useParams } from 'react-router-dom';
+import apiClient from '@/Libs/Https/API-client';
+import { processAPIResponse } from '@/Utils/CommonBaseClass';
+import CustomTextField from '@/components/CustomTextfield/CustomTextField';
+import { useForm } from 'react-hook-form';
+import CustomDatePicker from '@/components/CustomDatePicker/CustomDatePicker';
+import CustomSelect from '@/components/CustomSelectBox/CustomSelect';
+import moment from 'moment';
+import CustomButton from '@/components/CustomButton/CustomButton';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+import CustomSnackbar from '@/components/CustomSnackbar/CustomSnackbar';
+import routes from '@/router/routes';
+
+
+/**
+ * Coupon Details Page
+ * @author Neethu
+ */
+const CouponView: React.FC = () => {
+  const { id } = useParams<{ id: string }>(); // Retrieve the ID from URL parameters
+  const [coupon, setCoupon] = useState<Coupon | null>(null); // Replace 'Coupon' with your actual coupon type
+  const [loading, setLoading] = useState(true); // State for loading
+  const [error, setError] = useState<string | null>(null); // State for error message
+  const [editable, setEditable] = useState<boolean | null>(false); // State for error message
+  interface Coupon {
+    name: string;
+    description: string;
+    code: string;
+    startDate: Date; // You may want to use Date type
+    endDate: Date; // You may want to use Date type
+    discountType: string;
+    discountValue: number;
+    maxUses: number;
+    maxDiscountValue: number;
+    minPurchaseValue: number;
+  }
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');
+  const navigate = useNavigate();
+  const { control, handleSubmit, reset, getValues } = useForm();
+  /** 
+   * useEffect hook to handle the API call 
+   */
+  useEffect(() => {
+    fetchCoupon();
+  }, [id, reset]);
+
+  /**
+   * Fetch Coupon Details
+   */
+  const fetchCoupon = async () => {
+    try {
+      setLoading(true);
+      const response = await apiClient.get(`/coupon/${id}`); // Adjust the endpoint as needed
+
+      const { status, data, message } = await processAPIResponse(response, "Viewcoupon");
+      if (status) {
+        setCoupon(data);
+        reset(data);
+      }
+    } catch (err) {
+      setError('Failed to fetch coupon details.'); // Handle error appropriately
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  /** 
+   * save edited coupon
+   */
+  const updateCouponDetails = async () => {
+    try {
+      setLoading(true);
+      if (!coupon) {
+        return;
+      }
+
+      // Compare and filter only updated values
+      const updatedFields: Partial<Coupon> = {};
+      Object.keys(coupon!).forEach((key) => {
+        if (coupon[key as keyof Coupon] !== getValues(key as keyof Coupon)) {
+          updatedFields[key as keyof Coupon] = getValues(key as keyof Coupon);
+        }
+      });
+
+      if (Object.keys(updatedFields).length > 0) {
+        const response = await apiClient.put(`/coupon/${id}`, updatedFields); // Only send updated fields
+        const { status, data } = await processAPIResponse(response, "Viewcoupon");
+        if (status) {
+          setSnackbarOpen(true);
+          setSnackbarMessage("Coupon Updated Successfully");
+          setSnackbarSeverity('success');
+
+          setTimeout(() => {
+            navigate(routes.coupon()); // Redirect to the coupon list
+          }, 1500);
+        
+        }
+      } 
+
+
+    } catch (err) {
+      setError('Failed to update coupon details.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+  // Snackbar for displaying errors
+  const handleCloseSnackbar = () => {
+    setError(null);
+  };
+  const discountTypeOptions = [
+    { value: 'percentage', label: 'Percentage' },
+    { value: 'flat', label: 'Flat Rate' },
+  ];
+  return (
+    <>
+      <Box className="create-coupon-container">
+        <CustomSnackbar
+          open={snackbarOpen}
+          message={snackbarMessage}
+          severity={snackbarSeverity}
+          onClose={handleSnackbarClose}
+        />
+        {loading ? (
+          <CircularProgress />
+        ) : error ? (
+          <Snackbar open={!!error} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+            <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+              {error}
+            </Alert>
+          </Snackbar>
+        ) : (
+
+          <Grid container size={{ xs: 12, sm: 12 }} justifyContent='center' alignItems='center' spacing={4}>
+            <Grid size={{ xs: 12, sm: 6 }} className="create-coupon-grid">
+              <Grid size={{ xs: 12, sm: 12 }}>
+                <CustomButton
+                  className="custom-list-edit-btn"
+                  label='Edit'
+                  variant='contained'
+                  size='large'
+                  type='submit'
+                  startIcon={<EditIcon />}
+                  onClick={() => {
+                    setEditable(true);
+                  }} />
+              </Grid>
+              <Grid size={{ xs: 12, sm: 12 }}>
+                <Typography textAlign={'center'} lineHeight={2} className='create-coupon-title'>
+                  Coupon
+                </Typography>
+              </Grid>
+              <Grid size={{ xs: 12, sm: 12 }} className="create-coupon-form">
+                <form>
+                  <Grid container spacing={2} alignItems={'center'} justifyContent={'center'}>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomTextField
+                        name='name'
+                        placeholder='Coupon Name'
+                        control={control}
+                        defaultValue={coupon?.name}
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomTextField
+                        name='code'
+                        placeholder='Coupon Code'
+                        control={control}
+                        defaultValue={coupon?.code}
+                        rules={{ required: 'Coupon Code is required' }}
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomSelect
+                        name='discountType'
+                        label='Discount Type'
+                        defaultValue={coupon?.discountType}
+                        control={control}
+                        options={discountTypeOptions}
+                        fullWidth
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomTextField
+                        name='discountValue'
+                        placeholder='Discount Value'
+                        control={control}
+                        rules={{ required: 'Discount Value is required' }}
+                        type='number'
+                        defaultValue={coupon?.discountValue}
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomDatePicker
+                        placeholder='Start Date'
+                        name='startDate'
+                        control={control}
+                        defaultValue={coupon?.startDate ? moment(coupon.startDate).format('YYYY-MM-DD') : null}
+                        rules={{ required: 'Start Date is required' }}
+                        label='Start Date'
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomDatePicker
+                        placeholder='Expiry Date'
+                        name='endDate'
+                        defaultValue={coupon?.endDate ? moment(coupon.endDate).format('YYYY-MM-DD') : null}
+                        control={control}
+                        rules={{ required: 'Expiry Date is required' }}
+                        label='End Date'
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomTextField
+                        name='maxUses'
+                        placeholder='Maximum Usage'
+                        control={control}
+                        type='number'
+                        defaultValue={coupon?.maxUses}
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6 }}>
+                      <CustomTextField
+                        name='maxDiscountValue'
+                        placeholder='Maximum Discount Amount'
+                        control={control}
+                        defaultValue={coupon?.maxDiscountValue}
+                        type='number'
+                        requiredField
+                        disabled={!editable}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12 }}>
+                      <CustomTextField
+                        name='minPurchaseValue'
+                        placeholder='Minimum Purchase Amount'
+                        control={control}
+                        type='number'
+                        requiredField
+                        disabled={!editable}
+                        defaultValue={coupon?.minPurchaseValue}
+                      />
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 12 }}>
+                      <CustomTextField
+                        name='description'
+                        multiline={true}
+                        rows={4}
+                        defaultValue={coupon?.description}
+                        placeholder='Description'
+                        control={control}
+                        disabled={!editable}
+                      />
+                    </Grid>
+                  </Grid>
+                  {editable ? (
+                    <Grid size={{ xs: 12, sm: 12 }}>
+                      <CustomButton
+                        className="custom-list-save-btn"
+                        label='Save'
+                        variant='contained'
+                        size='large'
+                        type='submit'
+                        startIcon={<SaveIcon />}
+                        onClick={() => {
+
+                          updateCouponDetails();
+                          setEditable(false);
+                        }} />
+                    </Grid>) : ''}
+                </form>
+              </Grid>
+            </Grid>
+          </Grid>
+        )}
+      </Box>
+
+    </>
+  );
+};
+
+export default CouponView;
--- src/pages/coupon/index.tsx
+++ src/pages/coupon/index.tsx
@@ -1,7 +1,204 @@
 
+import { DataGridList } from "@/components/DataGrid/DataGridList"
+import routes from "@/router/routes";
+import Grid from '@mui/material/Grid2';
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
+import TuneRoundedIcon from '@mui/icons-material/TuneRounded';
+import FilterModal from "@/components/CustomFilter/FilterModal";
+import CustomAutocomplete from "@/components/CustomAutocomplete/CustomAutocomplete";
+import { useForm } from "react-hook-form";
+import apiClient from "@/Libs/Https/API-client";
+import { processAPIResponse } from "@/Utils/CommonBaseClass";
+import CustomButton from "@/components/CustomButton/CustomButton";
+import { Typography } from "@mui/material";
+import { ISource } from "@/Libs/type";
+
+
+interface FilterType {
+  id?: number;
+  name?: string;
+  // Add other filter fields here if needed
+}
+
+/**
+ * Used to render coupon list
+ * @author Neethu 
+ */
 const Coupon = () => {
+  const navigate = useNavigate();
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [filters, setFilters] = useState<FilterType>({});
+  const [source, setSource] = useState<ISource | undefined>(undefined);
+  const [loading, setLoading] = useState(false); // To indicate loading state for API
+
+  const { control } = useForm();
+  /**
+  * Useeffect hook handles the api call 
+  */
+  useEffect(() => {
+    if (!filters.id) {
+      couponList();
+    }
+  }, [])
+  // const [filterValue, setFilterValue] = useState(''); 
+  /**
+  *  Fetch summary balance when the component mounts
+  */
+  const couponList = useCallback(() => {
+    const req = {
+      offset: 0,
+      limit: 5,
+      filters: filters
+    };
+
+    setSource({
+      method: 'POST',
+      data: req,
+      url: `coupon/list`,
+      listName: 'couponList'
+    })
+    return;
+  }, []);
+
+  const columns = [
+    { type: 'default', field: 'id', headerName: "ID", width: 200 },
+    { type: 'default', field: 'name', headerName: "Coupon Name", width: 200 },
+    { type: 'default', field: 'discountType', headerName: "Type", width: 250 },
+    { type: 'default', field: 'endDate', headerName: "Expiry Date", width: 250 }
+  ]
+  /**
+   * Apply filter 
+   * @param newFilters 
+   */
+  const handleApplyFilters = (newFilters: any) => {
+    // Update filters when modal is applied
+    setSource({
+      method: 'POST',
+      data: {
+        offset: 0,
+        limit: 5,
+        filters: {
+          ...newFilters,
+        },
+      },
+      url: `coupon/list`,
+      listName: 'couponList',
+    });
+    setFilters(newFilters);
+  };
+
+  /**
+   * Row click navigation
+   */
+  const handleRowClick = (id: number | string) => {
+    navigate(routes.CouponView(id));
+  }
+  // Function to handle search API for autocomplete
+  const handleSearch = async (query: string) => {
+    setLoading(true);
+    try {
+      const req = {
+        filters: {
+          name: query
+        }
+      };
+      const response = await await apiClient.post(`coupon/list`, req);
+      const { status, data, message } = await processAPIResponse(response, 'couponList');
+      if (status) {
+        setSearchResults(data);
+      }
+      // Update the options based on API response
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // Function to handle search API for autocomplete
+  // New handler for when a coupon is selected from autocomplete
+  const handleAutocompleteChange = (selected: any) => {
+    //setSelectedCoupon(selected); // Update selected coupon
+    if (selected) {
+      // Here you can filter the data grid based on the selected coupon
+      setSource({
+        method: 'POST',
+        data: {
+          offset: 0,
+          limit: 5,
+          filters: {
+            id: selected.id, // Assuming the selected coupon has an 'id'
+          },
+        },
+        url: `coupon/list`,
+        listName: 'couponList',
+      });
+      setFilters({
+        id: selected.id, 
+      });
+    }
+  };
   return (
-    <div>Coupon</div>
+    <Grid container className="custom-list">
+      <Grid size={{ xs: 4 }} >
+        <Typography className='custom-list-list-title' gutterBottom>
+          Coupon
+        </Typography>
+      </Grid>
+
+      {/* Buttons for 'Create New Coupon' and 'Filters' */}
+      <Grid container size={{ xs: 8 }} spacing={2} justifyContent='flex-end'  >
+        <Grid container>
+          <CustomAutocomplete
+            name="search"
+            className="custom-search-text-field"
+            control={control}
+            placeholder="Search Coupon Name"
+            options={searchResults} // Dynamic options based on API results
+            getOptionLabel={(option: any) => option.name || ''} // Adjust based on your data structure
+            onSearch={handleSearch} // Call the search function
+            loading={loading}
+            onChange={handleAutocompleteChange}
+          />
+
+        </Grid>
+        <Grid container spacing={2}>
+          <CustomButton
+            className="custom-list-next-btn"
+            label='Create New Coupon'
+            variant='contained'
+            size='large'
+            type='submit'
+            startIcon={<AddIcon />}
+            onClick={() => {
+              navigate(routes.createCoupon());
+            }}
+          // disabled={loading}
+          />
+          <CustomButton
+            className="custom-list-filter-btn"
+            onClick={() => setIsFilterModalOpen(true)}
+            label="Filters"
+            startIcon={<TuneRoundedIcon />}
+            variant="contained"
+            color="primary"
+            size="large"
+          />
+        </Grid>
+      </Grid>
+      <Grid size={{ xs: 12 }} >
+        <DataGridList source={source} onRowClick={(params: any) => handleRowClick(params.id)} title="Coupon" hideFooterPagination={false} columns={columns} id="coupon-datagrid" />
+      </Grid>
+
+      {/* Filter Modal */}
+      <FilterModal
+        open={isFilterModalOpen}
+        onClose={() => setIsFilterModalOpen(false)}
+        onApplyFilters={handleApplyFilters}
+      />
+    </Grid>
   )
 }
 
