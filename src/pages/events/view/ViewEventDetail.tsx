0bda1681ed6a7a0871804f5818ec9f37e7de8942
AthulPP <ctr.athul.p@prth.com>
2024-10-30 15:03:32 +0530

    event detail contributor add changes

--- src/pages/events/view/ViewEventDetail.tsx
+++ src/pages/events/view/ViewEventDetail.tsx
@@ -210,7 +210,7 @@ const ViewEventDetail = () => {
             <EventInfoCard eventData={eventFullData} />
           </TabPanel>
           <TabPanel value="2">
-            <SepekerCard eventData={eventData} />
+            <SepekerCard eventData={eventFullData} />
           </TabPanel>
           <TabPanel value="3">
             <Sessions eventData={eventFullData} />
