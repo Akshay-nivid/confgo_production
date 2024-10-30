a8ab4434ac57791f3343f8d15cacc67805074efe
ranees <ranees@nivid.co>
2024-10-30 18:04:16 +0530

    changesin locationCard

--- src/pages/events/view/LocationCard.tsx
+++ src/pages/events/view/LocationCard.tsx
@@ -3,48 +3,62 @@ import { useEffect, useState } from "react";
 import apiClient from "@/Libs/Https/API-client";
 import { processAPIResponse } from "@/Utils/CommonBaseClass";
 import { useParams } from "react-router-dom";
+import { Logger } from "@/Utils/Logger";
+import useStore from "@/Libs/store";
 
 interface Location {
   mapUrl: string;
 }
 
+/**
+ * used to list the location in map
+ */
 const LocationCard = () => {
   const { id } = useParams();
   const [locations, setLocations] = useState<Location[]>([]);
   const [loading, setLoading] = useState(false);
-
+  const setDataById = useStore((state: any) => state.setDataById);
+  /**
+   * useEffect hook to handle the API call
+   */
   useEffect(() => {
-    const fetchLocationList = async () => {
-      setLoading(true);
-      try {
-        const response = await apiClient.get(`event/${id}`);
-        const { status, data } = processAPIResponse(response, "locationList");
+    fetchLocationList();
+  }, []);
 
-        const locationsData = status
-          ? Array.isArray(data)
-            ? data
-            : [data]
-          : [];
+  // fetch the venue details
+  const fetchLocationList = async () => {
+    setLoading(true);
+    try {
+      const response = await apiClient.get(`event/${id}`);
+      const { status, data } = processAPIResponse(response, "locationList");
+
+      if (status) {
+        const locationsData = Array.isArray(data) ? data : [data];
 
         setLocations(
           locationsData.map((item) => {
-            // Extracts the actual URL from mapUrl using regex, defaulting to Google Maps if unavailable
+            // Extract the URL from mapUrl, or set a default if unavailable
             const mapUrl = item.venue?.mapUrl || "https://maps.google.com";
-            return {
-              id: item.id,
-              name: item.name,
-              mapUrl,
-            };
+            return { mapUrl };
           })
-        );
-      } catch (error) {
-        console.error("Error fetching locations:", error);
-      } finally {
+        ); 
+        
+      } else {
+        // If `status` is false, display an error message
+        setDataById("snackBarInfo", {
+          open: true,
+          autoHideDuration: 2000,
+          severity: "failed",
+          message: "Error in fetchinglocation",
+        });
+      }
+    } catch (error) {
+      Logger.error("Error fetching locations:", error);
+    }
+    finally {
         setLoading(false);
       }
-    };
-    fetchLocationList();
-  }, [id]);
+  };
 
   return (
     <Grid container spacing={4} className="location-grid">
@@ -52,7 +66,7 @@ const LocationCard = () => {
         <div>Loading...</div>
       ) : (
         locations.map(({ mapUrl }) => (
-          <Grid item xs={12} sm={6} md={3} key={id}>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }} key={id}>
             {mapUrl ? (
               <iframe
                 src={mapUrl}
