22c00db8c68b551c917d7136e15fbcedb8d899f3
Rahulratheesh37 <rahulratheesh37@gmail.com>
2024-11-04 17:47:37 +0530

    new ,changes in condact,edit location drawer

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -44,6 +44,7 @@ import RaziorPayIcon from "./razorpay.svg";
 import EventRegistrationSuccessIcon from "./event-registration-complete.svg";
 import QrIcon from "./qr.svg";
 import ForgotPasswordIcon from "./key-icon.svg";
+import EditButtonIcon from "./edit-icon.svg"
 export {
   CalenderIcon,
   EventIcon,
@@ -90,5 +91,6 @@ export {
   RaziorPayIcon,
   EventRegistrationSuccessIcon,
   QrIcon,
-  ForgotPasswordIcon
+  ForgotPasswordIcon,
+  EditButtonIcon
 };
