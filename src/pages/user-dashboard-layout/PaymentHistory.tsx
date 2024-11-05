cd7657be01b702da5fcde425f775841c68cb4c23
Rahulratheesh37 <rahulratheesh37@gmail.com>
2024-11-05 17:10:55 +0530

    payment history

--- src/pages/user-dashboard-layout/PaymentHistory.tsx
+++ src/pages/user-dashboard-layout/PaymentHistory.tsx
@@ -3,8 +3,8 @@ import Grid from "@mui/material/Grid2";
 function PaymentHistory() {
   return (
   <Grid container size={12} className="payment-history-container">
-     <Grid className="" justifyContent={"center"} container>
-     <Typography padding={"3rem"} bgcolor={"red"}>
+     <Grid className="" justifyContent={"center"} size={12}>
+     <Typography className="heading" padding={"3rem"} bgcolor={"red"}>
         payment History
      </Typography>
      </Grid>
