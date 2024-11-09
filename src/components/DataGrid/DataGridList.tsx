b47289c5111131ab25ce08da1a9e7be616f1d094
Adithyan_N <ctr.adithyan@prth.com>
2024-11-09 15:58:34 +0530

    datagrid changes

--- src/components/DataGrid/DataGridList.tsx
+++ src/components/DataGrid/DataGridList.tsx
@@ -18,12 +18,12 @@ type DefColumn = {
     width: number;
     renderCell?: (params: any) => JSX.Element;
 };
-  
+
 type DataGridListProps = {
     id: string;
     columns: DefColumn[];
     hideFooterPagination: boolean;
-    source?: ISource; 
+    source?: ISource;
     dataTransformer?: Function;
     data?: any;
     title?: string
@@ -39,10 +39,25 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
     const setDataById = useStore((state: any) => state.setDataById)
     const dataInfo = useStore((state: any) => state?.compData?.[id]) ?? [];
     const prevPageRef = useRef<any>();
-    const pageSize = dataInfo.pageSize || 5;
+    const pageSize = dataInfo.source?.data?.limit || 5;
     const currentPage = dataInfo.currentPage || 1;
     const [loading, setLoading] = useState(false); // Added loading state
-  
+
+    /**
+     * Method used to find screen height and set datagrid height
+     */
+    useEffect(() => {
+        const updateGridHeight = () => {
+            const root = document.documentElement;
+            const availableHeight = window.innerHeight - 250; // Adjust offset as needed
+            root.style.setProperty('--grid-height', `${availableHeight}px`);
+        };
+
+        updateGridHeight();
+        window.addEventListener('resize', updateGridHeight);
+        return () => window.removeEventListener('resize', updateGridHeight);
+    }, []);
+
     /**
     * Useeffect hook handles the api call 
     */
@@ -62,21 +77,21 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
      * @param source 
      */
     const handleApiCall = async (source: any) => {
-        try { 
+        try {
             setLoading(true);
             const response = await apiClient.post(source.url, source.data);
-            const { status, data, message } = await processAPIResponse(response, source.listName); 
+            const { status, data, message } = await processAPIResponse(response, source.listName);
             if (status) {
                 const pagination = response?.data?.pagination;
-                const processedData = dataTransformer 
-                    ? dataTransformer(subNode ? data?.[subNode] : data) 
+                const processedData = dataTransformer
+                    ? dataTransformer(subNode ? data?.[subNode] : data)
                     : subNode ? data?.[subNode] : data;
                 // Check if the data array is empty
                 if (!processedData || processedData.length === 0) {
                     setDataById(id, { pagination, source, data: null, count: 0, dataTransformer });
                 } else {
                     setDataById(id, { pagination: pagination, source: source, data: dataTransformer ? dataTransformer(subNode ? data?.[subNode] : data) : subNode ? data?.[subNode] : data, count: subNode ? data?.pagination?.total : pagination.total, dataTransformer: dataTransformer });
-               
+
                 }
             }
             else {
@@ -93,19 +108,12 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
      * Method to handle pagination and call API
      */
     const onPaginationChange = (pageSize: any, currentPage: any) => {
-        //setPageSize(dataInfo.source.data.limit);
         const newSource = { ...dataInfo?.source };
         const newSourceData = newSource['data'];
-        if (currentPage < prevPageRef.current) {
-
-            newSourceData['offset'] = currentPage === 1 ? currentPage - 1 : newSourceData['offset'] - newSourceData['limit'];
-        } else {
-            newSourceData['offset'] = currentPage === 1 ? currentPage - 1 : newSourceData['offset'] + newSourceData['limit'];
-        }
+        newSourceData.offset = newSourceData.limit * (currentPage - 1);
         prevPageRef.current = currentPage;
         newSourceData['limit'] = pageSize;
         newSource['data'] = newSourceData;
-
         handleApiCall(newSource);
     }
     /**
@@ -116,7 +124,7 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
             if (item.type === 'status') {
                 return {
                     ...item,
-                      cellClassName: 'default-label display-flex',
+                    cellClassName: 'default-label flex',
                     renderCell: (params: any) => <StatusComponent className="data-grid-status" value={params.value}
                     />
                 };
@@ -126,15 +134,15 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
                     ...item,
                     cellClassName: 'default-label'
                 };
-            }else if(item.type==='dateField'){
-               return{ 
-                ...item,
-                cellClassName:'default-label',
-                renderCell: (params: { value: any }) => <div>{moment(params.value).format(item.dateFormat)}</div>
-            }
+            } else if (item.type === 'dateField') {
+                return {
+                    ...item,
+                    cellClassName: 'default-label',
+                    renderCell: (params: { value: any }) => <div>{moment(params.value).format(item?.dateFormat ? item?.dateFormat : 'DD/MM/YYYY')}</div>
+                }
             }
             return item;
-            
+
         });
     }, [columns]);
 
@@ -142,16 +150,12 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
     * funtion to handles page limt 
     */
     const handlePageSizeChange = (newSize: number) => {
-        // setPageSize(newSize);
-        // setCurrentPage(1);
         onPaginationChange(newSize, 1);
     };
     /*
     * funtion to handles pagination 
     */
     const handlePageChange = (_event: any, newPage: number) => {
-        // setCurrentPage(newPage);
-
         onPaginationChange(pageSize, newPage);
     };
 
@@ -161,9 +165,9 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
      */
     function CustomPagination(props: any) {
         return (
-            <Grid container className="pagination" justifyContent="space-between" spacing={2} alignItems={"center"}>
-                <Grid display={'flex'} alignItems={'center'}>
-                    <Typography className='pagination-label'> Show result : </Typography>
+            <Grid container size={12} className="pagination" justifyContent="space-between" alignItems={"center"}>
+                <Grid container size={4} >
+                    <Typography className='pagination-label' alignSelf={'center'}> Show result : </Typography>
                     <Select
                         value={props.rowsPerPage}
                         defaultValue={10}
@@ -181,7 +185,7 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
                         ))}
                     </Select>
                 </Grid>
-                <Grid alignSelf={'flex-end'}>
+                <Grid container>
                     <Pagination
                         count={dataInfo.pagination.totalPages}
                         page={dataInfo.pagination.currentPage}
@@ -195,9 +199,9 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
     return (
         <Grid container className="custom-data-grid-grid">
             {loading ? (
-                <CircularProgress/>
-            ) : dataInfo?.data && dataInfo?.data?.length >0 ? (
-                <Grid style={{ width: '100%' }}>
+                <CircularProgress />
+            ) : dataInfo?.data && dataInfo?.data?.length > 0 ? (
+                <Grid>
                     <DataGrid
                         rows={dataInfo?.data?.rows || dataInfo?.data}
                         columns={renderColumn}
@@ -231,7 +235,7 @@ export const DataGridList: React.FC<DataGridListProps> = ({ id, columns, hideFoo
                     />
                 </Grid>
             ) : (
-                <NoRecords/>
+                <NoRecords />
             )}
         </Grid>
     );
