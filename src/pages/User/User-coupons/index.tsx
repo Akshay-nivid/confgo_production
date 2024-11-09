99793126de6175b8bedd2725eef80004b20a3cb2
Rahulratheesh37 <rahulratheesh37@gmail.com>
2024-11-09 15:29:18 +0530

    coupouns

--- src/pages/User/User-coupons/index.tsx
+++ src/pages/User/User-coupons/index.tsx
@@ -1,60 +1,10 @@
-import CustomAutocomplete from "@/components/CustomAutocomplete/CustomAutocomplete";
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
 import EventCard from "../Components/EventCard";
+import React from "react";
 
-const MyEventScreen: React.FC = () => {
-    const { control } = useForm();
-    const [searchResults, setSearchResults] = useState([]);
-    const [loading, setLoading] = useState(false);
+const MyCoupons: React.FC = React.memo(() => {
 
-    /**
-  * Function to handle search API for autocomplete
-  */
-    const handleSearch = async (query: string) => {
-        // setLoading(true);
-        try {
-            let req: any = {
-                filters: {
-                    name: query,
-                },
-            };
-            //   const response = await await apiClient.get(`event/list`, req);
-            //   const { status, data } = await processAPIResponse(response, "eventList");
-            //   if (status) {
-            // setSearchResults(data);
-            //   }
-            // Update the options based on API response
-        } catch (error) {
-            //   Logger.error(error, "EventList.tsx");
-        } finally {
-            //   setLoading(false);
-        }
-    };
-
-    /**
-   * Function to handle search API for autocomplete
-   *  New handler for when an event is selected from autocomplete
-   * @param selected
-   */
-    const handleAutocompleteChange = (selected: any) => {
-        if (selected) {
-            //   setSource({
-            //     method: "GET",
-            //     data: {
-            //       offset: 0,
-            //       limit: 5,
-            //       filters: {
-            //         id: selected.id, // Assuming the selected event has an 'id'
-            //       },
-            //     },
-            //     url: `event/list`,
-            //     listName: "eventList",
-            //   });
-        }
-    };
     // Sample event data
     const events = [
         { datetitle: '2023-12-15', title: 'Kick', location: 'Kannur' },
@@ -64,19 +14,7 @@ const MyEventScreen: React.FC = () => {
         { datetitle: '2023-12-19', title: 'Node.js Seminar', location: 'New York' },
         { datetitle: '2023-12-20', title: 'JavaScript Conference', location: 'Paris' },
     ];
-    const squareButtonLabels: string[] = ["View Certificate", "Event Recap"];
-    const handleButtonPress = () => {
-        console.log('button pressed here>>>>>>')
-    }
-    const handleSquareButtonClick = (index: number) => {
-        console.log('Clicked label at index:', index);
-        // You can add specific logic based on the index here.
-        if (index === 0) {
-            console.log('First label clicked');
-        } else if (index === 1) {
-            console.log('Second label clicked');
-        }
-    };
+
     return (
 
         <Grid className="my-coupoun" container spacing={2}>
@@ -84,18 +22,7 @@ const MyEventScreen: React.FC = () => {
                 <Grid size={{ xs: 6 }} >
                     <Typography className="my-coupoun-header">Coupons</Typography>
                 </Grid>
-                <Grid size={{ xs: 6 }}>
-                    <CustomAutocomplete
-                        name="search"
-                        className="custom-search-text-field"
-                        control={control}
-                        options={searchResults}
-                        getOptionLabel={(option: any) => option.name || ""}
-                        onSearch={handleSearch}
-                        loading={loading}
-                        onChange={handleAutocompleteChange}
-                    />
-                </Grid>
+
             </Grid>
             <Grid container spacing={2}>
                 {events.map((event, index) => (
@@ -109,15 +36,12 @@ const MyEventScreen: React.FC = () => {
                             viewButton={true}
                             datetitle={event.datetitle}
                             title={event.title}
-                            location={event.location}
-                            buttonPress={handleButtonPress}
-                            squareButtonLabels={squareButtonLabels}
-                            onSquareButtonClick={handleSquareButtonClick}
+                            location={event.location} squareButtonLabels={[]}                           
                         />
                     </Grid>
                 ))}
             </Grid>
         </Grid>
     );
-};
-export default MyEventScreen;
+});
+export default MyCoupons;
