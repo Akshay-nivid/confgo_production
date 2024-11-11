b8f7901904cd802f9bd743a47953c4bd3e4b6ce1
Maneesh.C <ctr.maneesh.c@prth.com>
2024-11-11 09:46:01 +0530

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -175,7 +175,7 @@ const router = createBrowserRouter([
           },
           {
             path: routes.calendar(),
-            element: <CalendarRoute />,
+            element: <CalendarRoute id="events-calendar" key="events-calendar"/>,
           },
         ],
       },
