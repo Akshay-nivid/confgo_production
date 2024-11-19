4cbbac463dbed04a5b1ad113bd850897f225a045
Rahulratheesh37 <rahulratheesh37@gmail.com>
2024-11-19 15:04:57 +0530

    commit

--- src/pages/user-dashboard-layout/UserDashboard.tsx
+++ src/pages/user-dashboard-layout/UserDashboard.tsx
@@ -2,7 +2,7 @@
 import Grid from '@mui/material/Grid2';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { CircularProgress, Divider, Typography } from "@mui/material";
+import { Box, CircularProgress, Divider, Typography } from "@mui/material";
 import DashboardCardItem from './DashboardCardItem';
 import { CalendarEventIcon, DownloadEventIcon, EventsSvg, HeartEventIcon, PaymentDashboardIcon, PaymentHistoryIcon } from '@/assets/svg';
 import React from 'react';
@@ -236,7 +236,7 @@ const UserDashboard: React.FC = React.memo(() => {
           </Typography>
 
           <Grid container >
-
+           <Box>
             <Grid size={12} mt={1} className="dashboard-left-profile-card-recent" onClick={() => navigate('/user/payment-history')}>
               <CalendarEventIcon fontSize={20} /> View Payment History
             </Grid>
@@ -248,8 +248,9 @@ const UserDashboard: React.FC = React.memo(() => {
             <Grid size={12} mt={1} className="dashboard-left-profile-card-recent" onClick={() => navigate('/user/my-event')} >
               <PaymentHistoryIcon fontSize={20} /> Download Tickets & Certificates
             </Grid>
-            <Divider className='dashboard-left-profile-card-recent-dividers' />
-          </Grid>
+            </Box>
+          
+            </Grid>
 
 
         </Grid>
