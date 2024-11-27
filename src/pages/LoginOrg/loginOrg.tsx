31ca8797221f971e1f2e8476b2ecb6c6abc48901
Adithyan_N <ctr.adithyan@prth.com>
2024-11-27 12:15:53 +0530

    fixes

--- src/pages/LoginOrg/loginOrg.tsx
+++ src/pages/LoginOrg/loginOrg.tsx
@@ -6,6 +6,7 @@ import CustomTextField from "@/components/CustomTextfield/CustomTextField";
 import routes from "@/router/routes";
 import useStore from "@/Libs/store";
 import apiClient from "@/Libs/Https/API-client";
+import { SignUpFlowIcon } from "@/assets/svg";
 
 
 /**
@@ -154,7 +155,7 @@ const LoginOrg = () => {
   return (
     <Box className="login-org-main-container">
       <Grid container className="grid-layout">
-        <Grid size={{ xs: 12, sm: 6 }} className="grid-left">
+        <Grid size={{ xs: 12, sm: 7 }} className="grid-left">
           <Grid className="left-content-wrapper">
             <Grid className="left-inner-content">
               <Grid className="left-header-wrapper">
@@ -210,19 +211,8 @@ const LoginOrg = () => {
             </Grid>
           </Grid>
         </Grid>
-        <Grid size={{ xs: 12, sm: 6 }} className="grid-right">
-          <Box className="right-image-container">
-            <Typography className="image-content-text">
-              <Typography className="paragraph">
-                {" "}
-                Unlock the Future of Conference{" "}
-              </Typography>
-              <Typography className="paragraph">
-                {" "}
-                Management – Join Us Today!{" "}
-              </Typography>
-            </Typography>
-          </Box>
+        <Grid container size={{ xs: 12, md: 5 }} className="grid-right">
+          <SignUpFlowIcon />
         </Grid>
       </Grid>
     </Box>
