eb80a3ab7943f355ad9705801bf8a2d440f5f7cf
Rahulratheesh37 <rahulratheesh37@gmail.com>
2024-12-06 12:34:41 +0530

    commit

--- src/pages/User/User-Otp/index.tsx
+++ src/pages/User/User-Otp/index.tsx
@@ -162,6 +162,7 @@ const UserOtp = () => {
         body: body
         , successCB: successCB,
         errorCB: (error: any) => {
+          Logger.error("Error in validate otp", error);
           setDataById("snackBarInfo", {
           open: true,
           autoHideDuration: 2000,
