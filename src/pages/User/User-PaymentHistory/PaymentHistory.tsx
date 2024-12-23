2a837bc38e924104506a4748d7d90d4c638ed355
Neethu <ctr.neethu.kp@prth.com>
2024-12-23 21:46:14 +0530

    payment history list fixes

--- src/pages/User/User-PaymentHistory/PaymentHistory.tsx
+++ src/pages/User/User-PaymentHistory/PaymentHistory.tsx
@@ -24,7 +24,7 @@ const PaymentHistory: React.FC = React.memo(() => {
       type: "dateField",
       field: "Date",
       headerName: "Date",
-      width: 180,
+      width: 140,
       dateFormat: "DD/MM/YYYY",
     },
     { type: "default", field: "amount", headerName: "Amount", width: 130 },
@@ -32,7 +32,7 @@ const PaymentHistory: React.FC = React.memo(() => {
       type: "status",
       field: "status",
       headerName: "Status",
-      width: 139
+      width: 120
     },
     {
       type: "custom",
@@ -59,7 +59,7 @@ const PaymentHistory: React.FC = React.memo(() => {
         status: item?.event?.statusId,
         createdOn: item?.createdOn,
         Receipt: <CustomButton label={"[Download]"} className="download-Receipt" />,
-        PaymentMethod: item.paymentMethodId
+        PaymentMethod: item.paymentMethod.handler
 
       };
     });
