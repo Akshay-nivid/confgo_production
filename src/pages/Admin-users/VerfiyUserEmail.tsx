d2222ba9f76530b2084b78f4ec849faa8422793f
Athul <117159546+athulpp7@users.noreply.github.com>
2024-12-24 10:07:27 +0530

    create admin users

---
+++ src/pages/Admin-users/VerfiyUserEmail.tsx
@@ -0,0 +1,57 @@
+import apiClient from "@/Libs/Https/API-client";
+import useStore from "@/Libs/store";
+import routes from "@/router/routes";
+import { Logger } from "@/Utils/Logger";
+import CircularProgress from "@mui/material/CircularProgress";
+import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+
+const VerifyUSerMailPage = () => {
+    console.log('hiiiiiii')
+    const { token, id } = useParams<Record<string, string | undefined>>();
+    console.log('hiiiiiii',token,id)
+    const { setDataById }: any = useStore();
+    const decodedId = id ? atob(id) : '';
+    const navigate = useNavigate();
+    useEffect(() => {
+        verifyEmail();
+    }, [])
+    /**
+     * function used to verify email
+     */
+    const verifyEmail = async () => {
+        try {
+            const requestBody = {
+                userId: decodedId,
+                token: token,
+                type: "USER_REGISTRATION",
+            }
+            const response = await apiClient.post(`token/validatetoken`, requestBody)
+            if (response.data.status === 'success') {
+                setDataById('userDataRegister', { data:{userId:decodedId,token:token, tokenType: "USER_REGISTRATION"} });
+                navigate('/setpassword');
+            } else {
+                setDataById("snackBarInfo", {
+                    open: true,
+                    autoHideDuration: 2000,
+                    severity: "error",
+                    message: "This verification link has expired. Please request a new link.",
+                })
+                navigate(routes.loginOrg());
+            }
+        } catch (error) {
+          Logger.error(error,'VerifyMailPage.tsx')
+        }
+    };
+    return (
+        <Grid container justifyContent="center" alignItems="center" height="100vh" gap={2}>
+            <CircularProgress />
+            <Typography>Verifying your email Please wait...</Typography>
+        </Grid>
+    );
+};
+
+export default VerifyUSerMailPage;
