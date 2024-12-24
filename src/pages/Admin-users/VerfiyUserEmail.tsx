e9b86e15f2e33076fa68ea3b9e7ebbcf8f0c4f0c
Athul <117159546+athulpp7@users.noreply.github.com>
2024-12-24 13:35:35 +0530

    code change in verify mail

--- src/pages/Admin-users/VerfiyUserEmail.tsx
+++ src/pages/Admin-users/VerfiyUserEmail.tsx
@@ -1,21 +1,22 @@
-import apiClient from "@/Libs/Https/API-client";
 import useStore from "@/Libs/store";
 import routes from "@/router/routes";
-import { Logger } from "@/Utils/Logger";
 import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
-
+/**
+*componet for verify user from email
+*/
 const VerifyUSerMailPage = () => {
-    console.log('hiiiiiii')
     const { token, id } = useParams<Record<string, string | undefined>>();
-    console.log('hiiiiiii',token,id)
     const { setDataById }: any = useStore();
     const decodedId = id ? atob(id) : '';
     const navigate = useNavigate();
+    const POST = useStore((state: any) => state.POST);
+    /**
+    *useEffect getting verify mail
+    */
     useEffect(() => {
         verifyEmail();
     }, [])
@@ -23,28 +24,23 @@ const VerifyUSerMailPage = () => {
      * function used to verify email
      */
     const verifyEmail = async () => {
-        try {
-            const requestBody = {
+        await POST({
+            url:'token/validatetoken',
+            body:{
                 userId: decodedId,
                 token: token,
                 type: "USER_REGISTRATION",
-            }
-            const response = await apiClient.post(`token/validatetoken`, requestBody)
-            if (response.data.status === 'success') {
+            },
+            id:'user-email-verification',
+            successCB: (_context: any) => {
                 setDataById('userDataRegister', { data:{userId:decodedId,token:token, tokenType: "USER_REGISTRATION"} });
                 navigate('/setpassword');
-            } else {
-                setDataById("snackBarInfo", {
-                    open: true,
-                    autoHideDuration: 2000,
-                    severity: "error",
-                    message: "This verification link has expired. Please request a new link.",
-                })
+            }, 
+            errorCB: (context: any) => {
+                setDataById('snackBarInfo', { open: true, autoHideDuration: 2000, severity: 'error', message: context?.message });
                 navigate(routes.loginOrg());
             }
-        } catch (error) {
-          Logger.error(error,'VerifyMailPage.tsx')
-        }
+        });
     };
     return (
         <Grid container justifyContent="center" alignItems="center" height="100vh" gap={2}>
