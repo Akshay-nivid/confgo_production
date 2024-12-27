6d6493bb7b1e042744482e123284f3755f7f14d9
sham-l <shameel@nivid.co>
2024-12-27 16:05:35 +0530

    new file:   src/pages/Reviewer/ReviewerNavbar.tsx 	new file:   src/pages/Reviewer/details-page/index.tsx 	new file:   src/pages/Reviewer/details-page/style.scss 	new file:   src/pages/Reviewer/home/index.tsx 	new file:   src/pages/Reviewer/home/style.scss 	new file:   src/pages/Reviewer/style.scss

---
+++ src/pages/Reviewer/details-page/index.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import Grid from "@mui/material/Grid2";
+import { Typography, IconButton, Box, Rating } from '@mui/material';
+import { Book, DowloadIcon, PdfIcon } from '@/assets/svg';
+import StatusComponent from '@/components/Status/StatusComponent';
+import CustomButton from '@/components/CustomButton/CustomButton';
+import ReactQuill from 'react-quill';
+import ReviewerNavbar from "../ReviewerNavbar";
+
+export default function Reviewer() {
+    return (
+        <Box className="reviewer-main">
+
+            <ReviewerNavbar />
+
+            <Grid container columnSpacing={2}>
+
+                <Grid size={11} container className="layout-container">
+
+                    <Grid size={7}>
+                        <Box className="document-card">
+
+                            <Box className="eye-download-container">
+
+                                <Box className="header-container">
+                                    <Book />
+                                    <Typography>Abstract</Typography>
+                                </Box>
+
+                                <IconButton>
+                                    <DowloadIcon />
+                                </IconButton>
+
+                            </Box>
+
+
+
+                            <Box className="file-container">
+
+                                <PdfIcon />
+                                <Box className="file-info">
+                                    <Typography className='date'>December 15, 2024</Typography>
+                                    <Typography className='name'>NetworkingAbstract_200.pdf</Typography>
+                                </Box>
+
+                            </Box>
+
+                        </Box>
+
+                        <Box className="reviewer-message-container">
+
+                            <Box className="reviewer-avatar-container">
+                                {/* <Avatar className='avatar'>JD</Avatar> */}
+                                <Typography className='name'>Review</Typography>
+                            </Box>
+
+                            <Box className="rating-container">
+                                <Rating size='large' precision={0.5} />
+                            </Box>
+
+                            <Box className="editor-container">
+
+                                <ReactQuill modules={{
+                                    toolbar: [['bold', 'italic', 'link']]
+                                }}
+                                    placeholder='Write a comment...'
+                                />
+
+                            </Box>
+
+                            <Box className="editor-btn-container">
+                                <Box className='editor'></Box>
+                                <CustomButton label='Submit' className='comment-btn' />
+                            </Box>
+
+                        </Box>
+
+                    </Grid>
+
+                    <Box className="divider"></Box>
+
+
+                    <Grid size={"grow"} className="right-grid abstract-user-info-container">
+                        <Typography className='abstract-name'>Abstract Name</Typography>
+                        <Box className="user-info-container">
+                            <Box className="user-info-group">
+                                <Typography className='title'>Submitted By</Typography>
+                                <Typography className='value'>Dr. John Smith</Typography>
+                            </Box>
+                            <Box className="user-info-group">
+                                <Typography className='title'>Email</Typography>
+                                <Typography className='value'>test@gmail.com</Typography>
+                            </Box>
+                            <Box className="user-info-group">
+                                <Typography className='title'>Submission Date</Typography>
+                                <Typography className='value'>December 15, 2024</Typography>
+                            </Box>
+                            <Box className="user-info-group">
+                                <Typography className='title'>Status</Typography>
+                                <Box className="value">
+                                    <StatusComponent value='3' className='w-max' />
+                                </Box>
+                            </Box>
+                        </Box>
+
+                    </Grid>
+
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+
+
+
