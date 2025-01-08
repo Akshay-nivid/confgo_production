f0ff16cd85d632d830052dc858d1b9465c403059
Maneesh.C <ctr.maneesh.c@prth.com>
2025-01-08 17:33:46 +0530

    Save as draft changes

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -145,10 +145,10 @@ const Events = () => {
 
       const req = createFormRequest(formDraftData, true)
       const response = await apiClient.post('event', req);
-      const { status,  message } = await processAPIResponse(response, 'event-status');
+      const { status, data, message } = await processAPIResponse(response, 'event-status');
       if (status) {
         setDataById('snackBarInfo', { open: true, autoHideDuration: 2000, severity: 'success', message:message});
-        navigate(routes.events());
+        navigate(routes.editDraftEvent(data?.id))
       }
       else {
         setDataById('snackBarInfo', { open: true, autoHideDuration: 2000, severity: 'error', message:message});
