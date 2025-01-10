58d838bea141d60a81863e580bc2f65a76525cd0
Rahulratheesh37 <rahulratheesh37@gmail.com>
2025-01-10 11:59:54 +0530

    commit

--- src/pages/LoginOrg/loginOrg.tsx
+++ src/pages/LoginOrg/loginOrg.tsx
@@ -114,6 +114,7 @@ const LoginOrg = () => {
       sessionStorage.setItem('subscriptionStatus', subscriptionStatus);
       sessionStorage.setItem('acceptedTerms', acceptedTerms.toString());
       sessionStorage.setItem('companyId', companyId);
+      setDataById("AdminCompanyId",{companyId});
       sessionStorage.setItem('companyEmail', email);
       sessionStorage.setItem('companyPhone', phone);
       // Show success notification
