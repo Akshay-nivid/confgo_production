b536b8c0f9ec940daa49f2f447d199b6712e08c3
Maneesh.C <ctr.maneesh.c@prth.com>
2025-01-26 19:11:50 +0530

    New Template

--- src/pages/events/approvedTemplate/Template4.tsx
+++ src/pages/events/approvedTemplate/Template4.tsx
@@ -17,6 +17,14 @@ import config from "../../../../config.json";
 import TimeComponent from '../template/TimeComponent';
 import { ProgramDetailsModal } from '../template/_components';
 import CustomButton from '@/components/CustomButton/CustomButton';
+import SpeakerDetailsModal from '../template/_components/SpeakerDetailsModal';
+import NoProfilePicture from "../../../assets/svg/NoProfilePicture.svg";
+import ViewMoreLink from "../../../assets/svg/view-more.svg";
+import { setDataById, setNonPersistedDataById, snackBar } from '@/Libs/store';
+import routes from '@/router/routes';
+import { useNavigate } from 'react-router-dom';
+import SponsorShip from '../template/sponsorShipForm/SponsorShip';
+
 
 type TemplateViewProps = {
     data: any;
@@ -31,11 +39,14 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
     const contributorsRef = useRef(null);
     const programRef = useRef(null);
     const LocationRef = useRef(null);
+    const tierRef = useRef(null);
+    const beSponsorRef = useRef(null);
     const baseUrl = config.api.url;
     const [day, setDay] = useState<string>('');
     const [hour, setHour] = useState<string>('');
     const [minute, setMinute] = useState<string>('');
     const [second, setSecond] = useState<string>('');
+    const navigate = useNavigate();
 
   /**
     * Callback function to receive the updated time values from TimerCounterComp
@@ -111,15 +122,176 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
     * Program Tab date click handles
     * @param event, newValue
     */
-    // function handleProgramCardClick(item:any) {
-    //     setNonPersistedDataById('isProgramDetailsModelOpen', { value: true })
-    //     setNonPersistedDataById('programDetails', { value: item })
-    //   }
+    function handleProgramCardClick(item:any) {
+        setNonPersistedDataById('isProgramDetailsModelOpen', { value: true })
+        setNonPersistedDataById('programDetails', { value: item })
+      }
       let generalAddsOn = data?.addons?.filter((item: { startTime: any; endTime: any; }) => !item.startTime || !item.endTime);
+
+
+       /**
+     * Extracts unique speakers from a given list based on their `userId`.
+     * 
+     * @param {any[]} speakers - An array of speaker objects. Each object is expected to have a `userId` property.
+     * @returns {any[]} An array of unique speaker objects, ensuring no duplicate `userId`s.
+     * 
+     * This function uses a Map to track speakers by their `userId`. 
+     * It ensures that only one speaker per `userId` is included in the returned array.
+     */
+    function getUniqueSpeakers(speakers: any) {
+        const uniqueSpeakersMap = new Map<number, any>();
+      
+        speakers.forEach((speaker: any) => {
+          if (!uniqueSpeakersMap.has(speaker.userId)) {
+            uniqueSpeakersMap.set(speaker.userId, speaker);
+          }
+        });
+      
+        return Array.from(uniqueSpeakersMap.values());
+      }
+
+      function handleSpeakerCardClick(item:any) {
+    
+        setNonPersistedDataById('isSpeakerDetailsModelOpen', { value: true })
+        setNonPersistedDataById('speakerDetails', { value: item })
+    
+      }
+
+      /**
+         * Method calculates the total amount
+         * @param amountData : event data
+         * @returns 
+         */
+      const calculateTotalAmount = (amountData: any) => {
+        let totalAmount = 0;
+
+        function traverse(node: any) {
+            // If the current node has an 'amount', add it to the total
+            if (node.amount) {
+                totalAmount += parseFloat(node.amount) || 0;
+            }
+
+            // Recursively traverse through arrays or objects
+            for (const key in node) {
+                if (Array.isArray(node[key])) {
+                    node[key].forEach(traverse);
+                } else if (typeof node[key] === 'object' && node[key] !== null) {
+                    traverse(node[key]);
+                }
+            }
+        }
+
+        // Start traversal from the root
+        traverse(amountData);
+
+        return totalAmount;
+    }
+    /**
+     * Method groups the event price tiers array based on participant 
+     * @param array : event price tiers array
+     * @returns 
+     */
+    const groupByParticipantTypeId = (array: any) => {
+        return array.reduce((result: any, item: any) => {
+            // Use the participantTypeId as the key
+            const key = item.participantType.name;
+
+            // Initialize the group if it doesn't exist
+            if (!result[key]) {
+                result[key] = [];
+            }
+
+            // Add the item to the corresponding group
+            result[key].push(item);
+
+            return result;
+        }, {});
+    }
+
+    /**
+     * Method calculates the tier amount based on percentage and total amount
+     * @param groupedData : grouped event price tiers array 
+     * @param totalAmount : total amount
+     * @returns 
+     */
+    const calculateAmounts = (groupedData: any, totalAmount: any) => {
+        // Loop through each group
+        Object.keys(groupedData).forEach((participantTypeId) => {
+            groupedData[participantTypeId].forEach((item: any) => {
+                // Convert percentage to a decimal and calculate the amount
+                const percentage = 1 - (parseFloat(item.percentage) / 100);
+                item.calculatedAmount = totalAmount * percentage;
+            });
+        });
+        return groupedData;
+    }
+
+
+    /**
+     * Method transforms the start date and end date to Nov 12, 2024 - Dec 12, 2024 format
+     * @param startDate : start date
+     * @param endDate : end date
+     * @returns 
+     */
+    const formatDateRange = (startDate: any, endDate: any) => {
+
+        const options: any = { year: 'numeric', month: 'short', day: 'numeric' };
+        const start = new Date(startDate).toLocaleDateString('en-US', options);
+        const end = new Date(endDate).toLocaleDateString('en-US', options);
+        return `${start} - ${end}`;
+
+    }
+
+    /**
+    * Handles the click event of the 'Register' button
+    * by setting the participantTypeId in the store and navigating to the program selection page
+    * @param {object} tierData - contains the participantTypeId and tierName
+    */
+    function handleClickRegister(tierData: any) {
+
+        const userRole = sessionStorage.getItem('userRole')
+
+
+        const userToken = sessionStorage.getItem('token')
+
+        const startData = new Date(data?.startDate);
+
+
+        const isEventEnded = startData < new Date() 
+
+
+
+        if (isEventEnded) {
+            snackBar({ severity: 'error', message: "The event has ended." })
+            return
+        }
+
+
+
+
+
+        // admin user is perevented from navigating to cart
+        if (userToken && userRole !== 'USER') {
+            snackBar({ severity: 'error', message: 'please login using participant credentials' })
+            return
+        }
+
+        setDataById('participantTypeId', { value: tierData.participantTypeId });
+
+        navigate(routes.programSelection())
+    }
+
+
+
+    const totalAmount = calculateTotalAmount(data);
+
+    const amountCalculatedData = calculateAmounts(groupByParticipantTypeId(data?.eventPriceTiers), totalAmount);
+
+
     return (
         <Grid container size={{ xs: 12, sm: 12 }} className={`${classPrefix}-bg`}>
             <Grid container size={{ xs: 12, sm: 12 }} className={classPrefix}>
-                <TopMenuHeader links={headerLinks} classPrefix={`${classPrefix}-top-menu`} data={data} onScrollToProgram={() => handleScrollTo(programRef)} onScrollToAbout={() => handleScrollTo(aboutRef)} onScrollToContributors={() => handleScrollTo(contributorsRef)} onScrollToLocation={() => handleScrollTo(LocationRef)} />
+                <TopMenuHeader links={headerLinks} classPrefix={`${classPrefix}-top-menu`} data={data} onScrollToProgram={() => handleScrollTo(programRef)} onScrollToAbout={() => handleScrollTo(aboutRef)} onScrollToContributors={() => handleScrollTo(contributorsRef)} onScrollToLocation={() => handleScrollTo(LocationRef)} onScrollToBeSponsor={() => handleScrollTo(beSponsorRef)} />
                 <Grid container size={{ xs: 12, sm: 12 }} className={`${classPrefix}-header`} />
                 <AuthFormHandler className={`${classPrefix}-headerBottom`} data={data} />
                 <TEventDetails className={`${classPrefix}-eventDetails`} data={data} />
@@ -159,7 +331,43 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                         </TimerCounterComp>
                     </Grid>
                 </Grid>
-                {/* Program section */}
+                {/* Speaker section starts here  */}
+                {data?.eventSpeakers?.length > 0 && getUniqueSpeakers(data?.eventSpeakers)?.length > 0 && <Grid id={'Contributors'} container size={{ xs: 12, sm: 12 }} className={`${classPrefix}-event-contributors `} spacing={1} direction={'column'} justifyContent={'center'} alignItems={'center'} ref={contributorsRef}>
+                    <SpeakerDetailsModal />
+                    <Grid className={`${classPrefix}-event-contributors-title`}>Meet Our Esteemed Speakers</Grid>
+                    <Grid container size={{ xs: 12, sm: 12 }} className={`${classPrefix}-event-contributors-item-group-container`} justifyContent={'center'} alignItems={'center'} spacing={4}>
+                        {data?.eventSpeakers?.length > 0 && getUniqueSpeakers(data?.eventSpeakers)?.map((item: any) => {
+                            return <Grid alignSelf={'stretch'} onClick={() => handleSpeakerCardClick(item)} size={{ xs: 12, sm: 3 }} container direction={'row'} className={`${classPrefix}-event-contributors-item-container `} spacing={2}>
+                                <Grid size={{ xs: 12, sm: 12 }} container direction={'column'} className={`${classPrefix}-event-contributors-item-container-speaker-card `}>
+                                    <Grid overflow={'hidden'} className={`${classPrefix}-event-contributors-item-container-speaker-card-image-container`}>
+                                        {item?.user?.assetId ? (<img
+                                            height={'100%'}
+                                            width={'100%'}
+                                            src={`${baseUrl}asset/${item?.user?.assetId}`}
+                                            alt={item.name}
+                                        />) : (
+                                            <NoProfilePicture className='h-full w-full' />
+                                        )}
+
+                                    </Grid>
+                                    <Grid container direction={'column'} >
+                                        <Grid className={`${classPrefix}-event-contributors-item-name`}>{`${item.user?.firstName} ${item.user?.lastName}`}</Grid>
+                                        <Grid className={`${classPrefix}-event-contributors-item-designation`}>{item.user?.designation}</Grid>
+                                        <Grid className={`${classPrefix}-event-contributors-item-view-more`} ><CustomButton
+                        label={'View more'}
+                        className={`${classPrefix}-event-contributors-item-view-more-button`}
+                        onClick={handleSpeakerCardClick}
+                        endIcon={<ViewMoreLink />}
+                    /></Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        })}
+                    </Grid>
+                </Grid>}
+
+                {/* Speaker section ends here  */}
+                {/* Program section starts here */}
                 <Grid id={"Program"} container size={{ xs: 12, sm: 12 }} spacing={2} mb={10} ref={programRef}>
                     <Grid size={{ xs: 12 }} justifyContent={'center'} mt={{ xs: 2, sm: 4 }}><Typography className={`${classPrefix}-program-schedule-heading`}>Conference Program Schedule</Typography></Grid>
                     {/* <Grid container size={{xs:12}} justifyContent={'center'} alignItems="center" mb={{xs:2,sm:3}}>
@@ -211,7 +419,7 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                     </Grid>
                     <Grid container spacing={3} className={`${classPrefix}-program-content-container`} mt={2} direction="column" alignContent={'center'} size={{ xs: 12, sm: 12 }}>
                         {combinedAndSortedItems?.map((item: any, index: number) => (
-                            <Grid /*onClick={() => handleProgramCardClick(item)}*/ size={{ xs: 11 }} justifyContent={'center'} pl={{ xs: 2, md: 4 }} p={2} key={index}
+                            <Grid onClick={() => !item.addonId && handleProgramCardClick(item)} size={{ xs: 11 }} justifyContent={'center'} pl={{ xs: 2, md: 4 }} p={2} key={index}
                                 className={`${classPrefix}-program-content-item ${item?.type === 'program' ? `${classPrefix}-program-content-item-program` : `${classPrefix}-program-content-item-addon`}`}
                             >
                                 <Grid container alignItems="center" >
@@ -247,7 +455,7 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                                                 
                                                     alt={speaker?.user?.firstName}
                                                     src={speaker?.user?.assetId
-                                                        ? `${baseUrl}asset/${speaker?.assetId}`
+                                                        ? `${baseUrl}asset/${speaker?.user?.assetId}`
                                                         : ""}
                                                 />
                                             })}
@@ -265,7 +473,57 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                         <ProgramDetailsModal />
                     </Grid>
                 </Grid>
+                {/* Program section ends here */}
                 <LocationSection classPrefix={`${classPrefix}-location`}data={data} onScrollToTier={LocationRef}/>
+                {/* Registration and ticketing section starts here */}
+                <Grid ref={tierRef} container size={{ xs: 12, sm: 12 }} className={`${classPrefix}-ticketing`} justifyContent={'center'} alignItems={'center'} spacing={2} direction={'column'}>
+                    <Grid><Typography className={`${classPrefix}-ticketing-title`}>Registration & Ticketing</Typography></Grid>
+
+                    <Grid className={`${classPrefix}-ticketing-anim-container`} container spacing={2} justifyContent={'center'} alignContent={'center'}>
+                        {
+                            Object.keys(amountCalculatedData)
+                                .map((participantType: any) => {
+
+                                    return (
+
+                                        <Grid size={{ xs: 12, sm: 6 }} key={participantType} className={`${classPrefix}-ticketing-item-container anim-item`}>
+                                            <Grid container justifyContent={'center'}>
+                                                <Typography className={`${classPrefix}-ticketing-item-title`}>
+                                                    {participantType}
+                                                </Typography>
+                                            </Grid>
+                                            <Grid className={`${classPrefix}-ticketing-content-container`}>
+                                                {
+                                                    amountCalculatedData[participantType]
+                                                        .map((item: any) => {
+                                                            const dateRange = formatDateRange(item.startDate, item.endDate);
+                                                            return parseFloat(item?.percentage) > 0 ? (
+                                                                <Grid container className={`${classPrefix}-ticketing-sub-item-container`} justifyContent={'space-between'}>
+                                                                    <Grid container direction={'column'}>
+                                                                        <Grid><Typography className={`${classPrefix}-ticketing-sub-item-name`}>{item.name}</Typography></Grid>
+                                                                        <Grid><Typography className={`${classPrefix}-ticketing-sub-item-date`}>{dateRange}</Typography></Grid>
+                                                                    </Grid>
+                                                                    <Grid container alignItems={'center'}>
+                                                                        <Typography className={`${classPrefix}-ticketing-sub-item-amount`}>{parseFloat(item?.percentage)}% OFF</Typography>
+                                                                    </Grid>
+                                                                </Grid>
+                                                            ) : <></>;
+                                                        })
+                                                }
+                                            </Grid>
+                                            <Grid container justifyContent={'center'} alignItems={'flex-end'} className={`${classPrefix}-ticketing-register-button-container`}><CustomButton onClick={() => handleClickRegister(amountCalculatedData[participantType]?.[0])} label="Register Now" className={`${classPrefix}-ticketing-register-button`} /></Grid>
+
+                                        </Grid>)
+                                })
+                        }
+                    </Grid>
+                </Grid>
+                {/* Registration and ticketing section ends here */}
+                {/* Sponsor enquiry form starts here */}
+                <Grid minHeight={"max-content"} size={12} container ref={beSponsorRef}>
+                    <SponsorShip eventId={data?.id} />
+                </Grid>
+                {/* Sponsor enquiry form ends here */}
                 <FooterSection classPrefix={`${classPrefix}-footer`} data={data} />
             </Grid>
         </Grid>
