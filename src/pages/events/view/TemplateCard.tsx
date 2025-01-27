e1c7d3942b4853304770e5256360863871833eed
Maneesh.C <ctr.maneesh.c@prth.com>
2025-01-27 14:14:56 +0530

    Enable default template only

--- src/pages/events/view/TemplateCard.tsx
+++ src/pages/events/view/TemplateCard.tsx
@@ -68,7 +68,7 @@ const TemplateCard = (data: any) => {
       await POST({
         url: `template/list`,
         id: 'templateList',
-        body: { enabled: 1, limit: 4 },
+        body: { enabled: 1, limit: 1 },
         errorCB: (context: any) => {
           Logger.error('TemplateView.tsx', context?.message);
         }
