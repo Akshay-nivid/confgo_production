9cb9d4c24c4014a3042c90c49078ba6492bf9aaf
Rahulratheesh37 <rahulratheesh37@gmail.com>
2025-01-29 10:54:17 +0530

    commit

--- src/pages/events/approvedTemplate/Template4.tsx
+++ src/pages/events/approvedTemplate/Template4.tsx
@@ -4,7 +4,7 @@ import TopMenuHeader, { LinkData } from './TopMenuHeader';
 import AuthFormHandler from './AuthFormHandler';
 import TEventDetails from './TEventDetails';
 import TimerCounterComp from '../template/TemplateTimer/TimerCounterComp';
-import { getLocalTimeDate, groupByDate } from '@/Utils/CommonBaseClass';
+import { getLocalTimeDate, groupByDate, truncateString } from '@/Utils/CommonBaseClass';
 import { Avatar, Box,Divider, Typography } from '@mui/material';
 import LocationSection from '../template/LocationSection';
 import FooterSection from '../template/FooterSection';
@@ -424,7 +424,7 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                                     </Grid>
                                     <Grid container direction={'column'} >
                                         <Grid className={`${classPrefix}-event-contributors-item-name`}>{`${item.user?.firstName} ${item.user?.lastName}`}</Grid>
-                                        {item.user?.designation && <Grid className={`${classPrefix}-event-contributors-item-designation`}>{item.user?.designation}</Grid>}
+                                        {item.user?.designation && <Grid className={`${classPrefix}-event-contributors-item-designation`} title={item.user?.designation}>{truncateString(item.user?.designation, 30)}</Grid>}
                                         <Grid className={`${classPrefix}-event-contributors-item-view-more`} ><CustomButton
                                             label={'View more'}
                                             className={`${classPrefix}-event-contributors-item-view-more-button`}
@@ -599,20 +599,19 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                                                         </Grid>
                                               
                                                     </Grid>
-
-                                        
+                                                    
                                                      {subItem?.eventSponsors && subItem?.eventSponsors?.length!==0&& (  <Grid size={6} container justifyContent={"flex-end"} >
                                                         {/* Sponsors */}
                                                         {/* {subItem?.eventSponsors?.length !== 0 && subItem?.eventSponsors?.length < 2 && (!subItem?.eventSpeakers || subItem?.eventSpeakers?.length === 0) && ( */}
                                                      
-                                                            <Grid container className={`${classPrefix}-program-content-sponsor`}   >
+                                                            <Grid container className={`${classPrefix}-program-content-sponsor`}>
                                                                
                                                                 <Grid  container justifyContent={"center"} size={12} className={`${classPrefix}-program-content-sponsor-heading`}>
                                                                     <Typography>Sponsored by</Typography>
                                                                 </Grid>
 
                                                                 {subItem.eventSponsors.map((sponsor: any) => (
-                                                                    <Grid  size={6}key={sponsor.id} container  className={`${classPrefix}-program-content-sponsor-ImgBox`}>
+                                                                    <Grid  size={12}key={sponsor.id} container  className={`${classPrefix}-program-content-sponsor-ImgBox`} justifyContent={"flex-end"}>
                                                                         {sponsor?.sponsor?.logoAssetId ? (
                                                                             <img
                                                                                 alt={sponsor?.sponsor?.name}
