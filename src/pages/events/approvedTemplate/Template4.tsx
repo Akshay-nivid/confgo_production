53622a9f9881c8bfbd8cef60e1d084eeeb774ffe
Maneesh.C <ctr.maneesh.c@prth.com>
2025-01-29 13:27:19 +0530

    Modal changes

--- src/pages/events/approvedTemplate/Template4.tsx
+++ src/pages/events/approvedTemplate/Template4.tsx
@@ -554,7 +554,7 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                     </Grid>
                     <Grid container spacing={3} className={`${classPrefix}-program-content-container`} mt={2} direction="column" alignContent={'center'} size={{ xs: 12, sm: 12 }}>
                         {combinedAndSortedItems?.map((item: any, index: number) => (
-                            <Grid onClick={() => handleProgramCardClick(item)} size={{ xs: 11 }} justifyContent={'center'} pl={{ xs: 2, md: 4 }} p={2} key={index}
+                            <Grid size={{ xs: 11 }} justifyContent={'center'} pl={{ xs: 2, md: 4 }} p={2} key={index}
                                 className={`${classPrefix}-program-content-item ${item?.type === 'program' ? `${classPrefix}-program-content-item-program` : `${classPrefix}-program-content-item-addon`}`}
                             >
                                 <Grid container alignItems="center" size={12} >
@@ -581,7 +581,7 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) => {
                                     <Grid size={{ xs: 6, sm: 8 }} className={`${classPrefix}-program-content-details-${item.type === 'program' ? 'program' : 'addon'}`} container direction='column' flex={"column"}>
                                         {item.subItems && item.subItems.length > 0 ? (
                                             item.subItems.map((subItem: any, subIndex: number) => (
-                                                <Grid container key={subIndex} size={12} display={"flex"}   >
+                                                <Grid container key={subIndex} size={12} display={"flex"} onClick={() => handleProgramCardClick(subItem)}  >
                                                     <Grid size={6} container >
                                                         <Grid size={12}>
                                                             <TitleComponent
