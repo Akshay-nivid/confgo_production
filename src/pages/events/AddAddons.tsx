ca144943333b3665009d2421ebbe5a836af61fc1
ranees <ranees@nivid.co>
2025-01-30 12:22:08 +0530

    bug fix 613, 614

--- src/pages/events/AddAddons.tsx
+++ src/pages/events/AddAddons.tsx
@@ -774,6 +774,7 @@ const AddAddOns: React.FC<ProgramProps> = React.memo(
                       
                                   </>
                                 } </>}
+                                {watch(`addOn.${index}.dateRequired`) && (
                                 <Grid size={{ xs: 12, sm: 12 }} display={"flex"} justifyContent={"space-between"}>
                                   <Grid size={{ xs: 12, sm: 6 }}>
                                     <Grid container display={"flex"} alignItems={"center"}>
@@ -795,6 +796,7 @@ const AddAddOns: React.FC<ProgramProps> = React.memo(
                                   </Grid>
                                   </Grid>
                                 </Grid>
+                                )}
                                 <Grid size={{ xs: 12, sm: 12 }} display={"flex"} justifyContent={"space-between"}>
                                   {/* Conditionally render Number of Days field */}
                                   {watch(`addOn.${index}.repeat`)?.length > 0 && (
