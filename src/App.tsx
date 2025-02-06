db1e742f943d26f92c1b12d45eb0d3eba82108b7
Adithyan_N_ <ctr.adithyan.adithyan@prth.com>
2025-02-06 19:02:30 +0530

    Remove test page and route
    - Deleted unused Test.tsx page
    - Removed '/test' route from application routing

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,6 @@ import SpeakersEventDetails from "./pages/Speaker/Home/SpeakersEventDetails";
 import { AccountDeletionPage } from "./pages/Delete-Account";
 import Sponsors from "./pages/Admin/Sponsors";
 import AdminPaymentList from "./pages/Admin/Payment";
-import Test from "./pages/Test";
 
 
 const userRoutes = [
@@ -199,10 +198,7 @@ const userRoutes = [
 ];
 
 const router = createBrowserRouter([
-  {
-    path: '/test',
-    element: <Test/>
-  },
+ 
   // Public routes
   {
     path: routes.deleteAccount(),
