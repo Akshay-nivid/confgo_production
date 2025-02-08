2c5c942f4be8986702d087ec043898acd1c615be
Nevin <nevin@nivid.co>
2025-02-08 12:54:00 +0530

    basic info count card component

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -156,7 +156,12 @@ import AbstractSelectedGray from "./abstractselectedgrayicon.svg"
 import AbstractSelectedGreen from "./abstractselectedgreenicon.svg"
 import AbstractNonSelectedGray from "./abstractnonselectedgrayicon.svg"
 import AbstractNonSelectedGreen from "./abstractnonselectedgreenicon.svg";
-import EventLocation from "./event-locations.svg"
+import EventLocation from "./event-locations.svg";
+import EventDetailProgram from "./event-detail-programs.svg";
+import EventDetailSponsor from "./event-details-sponsor.svg";
+import EventDetailSpeaker from "./event-detail-Speaker.svg";
+import EventDetailAttendee from "./event-detail-attendence.svg";
+
 export {
   MainBottomBanner,
   MainYourBrandImg,
@@ -317,4 +322,8 @@ export {
   AbstractNonSelectedGreen,
   EventCalendar,
   EventLocation,
+  EventDetailProgram,
+  EventDetailSponsor,
+  EventDetailSpeaker,
+  EventDetailAttendee
 };
