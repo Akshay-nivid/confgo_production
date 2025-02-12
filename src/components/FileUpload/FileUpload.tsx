f920c660e6e1f7d5e21cbce4a610ee867e945476
Nevin <nevin@nivid.co>
2025-02-12 10:28:50 +0530

    Event gallery page

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -28,6 +28,7 @@ interface FileUploadProps {
   isAbstract?: boolean;
   disabled?: boolean;
   ratioLabel?: string;
+  NoRecommended?:boolean
 }
 
 /**
@@ -50,6 +51,7 @@ const FileUpload: React.FC<FileUploadProps> = ({
   isAbstract,
   disabled = false,
   ratioLabel = "16:9",
+  NoRecommended = false
 }) => {
   const [previewUrls, setPreviewUrls] = useState<string[]>([]);
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
@@ -235,7 +237,7 @@ const FileUpload: React.FC<FileUploadProps> = ({
               <>
               <Typography className="upload-dropzone-text">Drag & drop or click here to upload.</Typography>
               <Typography className="upload-dropzone-subtext">Choose a file to upload, Max file size: {maxSize}MB.</Typography>
-              <Typography className="upload-dropzone-subtext">Recommended ratio: {ratioLabel} for best fit</Typography>
+              { !NoRecommended && <Typography className="upload-dropzone-subtext">Recommended ratio: {ratioLabel} for best fit</Typography>}
               </>
             )}
           </Grid>
