72dbb5b8fdb72da7eb33fdcc0faaf2d3694fa787
Athul <117159546+athulpp7@users.noreply.github.com>
2025-02-15 18:46:19 +0530

    template banner app

--- src/pages/events/approvedTemplate/Template4.tsx
+++ src/pages/events/approvedTemplate/Template4.tsx
@@ -24,7 +24,7 @@ import routes from '@/router/routes';
 import { useNavigate } from 'react-router-dom';
 import SponsorShip from '../template/sponsorShipForm/SponsorShip';
 import TempHall from "../../../assets/svg/temp-hall.svg";
-import { personPlaceholder } from '@/assets/png';
+import { personPlaceholder, Tempalte4App } from '@/assets/png';
 import {YellowSeat, RedSeat} from '@/assets/svg/index';
 
 type TemplateViewProps = {
@@ -1052,6 +1052,22 @@ const Template4: React.FC<TemplateViewProps> = React.memo(({ data }) =>{
                     <SponsorShip eventId={data?.id} />
                 </Grid>
                 {/* Sponsor enquiry form ends here */}
+                {/* App banner Image */}
+                <Grid 
+                  container 
+                  justifyContent={"center"} 
+                  alignItems={"center"} 
+                  alignSelf={"center"}
+                  size={12}
+                  mb={8}
+                >
+                  <Grid  size={10}>
+                    <img 
+                      src={Tempalte4App} 
+                      alt="Template 4"
+                    />
+                  </Grid>
+                </Grid>
                 <FooterSection classPrefix={`${classPrefix}-footer`} data={data} />
             </Grid>
         </Grid>
