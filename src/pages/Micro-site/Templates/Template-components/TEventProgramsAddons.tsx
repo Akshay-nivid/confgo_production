5beb4f9d05c646f466d745992413a55ca130b602
Maneesh.C <ctr.maneesh.c@prth.com>
2025-02-25 12:14:27 +0530

    Build error fix

--- src/pages/Micro-site/Templates/Template-components/TEventProgramsAddons.tsx
+++ src/pages/Micro-site/Templates/Template-components/TEventProgramsAddons.tsx
@@ -1,9 +1,7 @@
-import useStore, { IStoreState } from '@/Libs/store';
-import { IEventResponse } from '@/Libs/types/event';
+
 
 const TEventProgramsAddons = () => {
 
-    const eventData:IEventResponse = useStore((state:IStoreState)=>state.compData?.event?.data);
 
 
   return (
