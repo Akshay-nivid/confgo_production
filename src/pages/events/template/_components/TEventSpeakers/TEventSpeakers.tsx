6f04cd158d5e84f0187005c6bbfb33827b6fb9b2
Athul <117159546+athulpp7@users.noreply.github.com>
2025-02-25 12:33:05 +0530

    template changes

--- src/pages/events/template/_components/TEventSpeakers/TEventSpeakers.tsx
+++ src/pages/events/template/_components/TEventSpeakers/TEventSpeakers.tsx
@@ -37,7 +37,7 @@ const TEventSpeakers = ({ ItemWrapper = 'div', usageType, children }: TEventSpea
     function getUniqueSpeakers(speakers: any) {
         const uniqueSpeakersMap = new Map<number, any>();
 
-        speakers.forEach((speaker: any) => {
+        speakers?.forEach((speaker: any) => {
             if (!uniqueSpeakersMap.has(speaker.userId)) {
                 uniqueSpeakersMap.set(speaker.userId, speaker);
             }
