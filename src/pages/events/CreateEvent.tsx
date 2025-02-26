668453005c29bd37245e1148cd3484299247768b
anju <anju@nivid.co>
2025-02-26 16:51:54 +0530

    new logo uploaded

--- src/pages/events/CreateEvent.tsx
+++ src/pages/events/CreateEvent.tsx
@@ -22,7 +22,7 @@ import confgo  from "../../../config.json"
 import PublicOffOutlinedIcon from '@mui/icons-material/PublicOffOutlined';
 import PublicOutlinedIcon from '@mui/icons-material/PublicOutlined';
 import RssFeedOutlinedIcon from '@mui/icons-material/RssFeedOutlined';
-import UploadLogo from '../../assets/svg/uploadLogo.svg'
+import UploadLogo from '../../assets/svg/Upload-logo.svg';
 import { Close } from "@mui/icons-material";
 import UploadedIcon from '../../assets/svg/CreateEventimageIcon.svg'; // Replace with your actual UploadedIcon
 import CustomDateTimePicker from "@/components/CustomDateTimePicker/CustomDateTimePicker";
@@ -733,8 +733,8 @@ const CreateEvent: React.FC<EventProps> =
   
             <Typography className="create-event-upload-box-container-button-text"> Upload Logo</Typography>
             <Typography variant="body2">
-              Choose a file to upload, Max file size: 5MB.<br />
-              Recommended ratio: 16:9 for best fit
+              Choose a file to upload, Max file size: 1MB.<br />
+              Recommended ratio Min.200 × 200 (Pixels)
             </Typography>
             </button>
           </Grid>
