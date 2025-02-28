4e3c13f206050ca9af81b543841d2506d991465c
Athul <117159546+athulpp7@users.noreply.github.com>
2025-02-28 10:17:18 +0530

    assets

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -172,6 +172,9 @@ import NewDrawerClose from "./new-drawer-close.svg";
 import SpeakerDashBoard from "./speaker-no-program.svg";
 import EnterpriceWhiteIcon from "./EntrepriseWhiteIcon.svg";
 import WhiteTickIcon from "./WhiteTickIcon.svg";
+import T7MapPin from "./t7-map-pin-line.svg";
+import T7Email from "./t7-mail-line.svg";
+import T7Phone from "./t7-phone.svg";
 export {
   MainBottomBanner,
   MainYourBrandImg,
@@ -346,4 +349,7 @@ export {
   SpeakerDashBoard,
   EnterpriceWhiteIcon,
   WhiteTickIcon,
+  T7MapPin,
+  T7Email,
+  T7Phone
 };
