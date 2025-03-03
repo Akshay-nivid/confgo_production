0fe7f115e2bef3cf4aec511eb7de1073988cfb65
Athul <117159546+athulpp7@users.noreply.github.com>
2025-03-03 10:57:46 +0530

    .

--- src/pages/Micro-site/Templates/Template-components/SponsorContactForm.tsx
+++ src/pages/Micro-site/Templates/Template-components/SponsorContactForm.tsx
@@ -128,7 +128,7 @@ const SponosrContactForm = ({Id,customStyle}:SponosrContactFormProps) => {
     }
     return (
         <Grid container className={clsx('sponsor-page',customStyle)} size={{ lg: 12 }}>
-            <Typography ml={1} variant='h4'>Become Sponsor</Typography>
+            <Typography className='form-header' ml={1} variant='h4'>Become Sponsor</Typography>
             <Grid container className='sponsor-content' size={12} spacing={2} >
                 <Grid container className='sponsor-content-wrapper' size={{ lg: 12, xs: 12 }} spacing={3} justifyContent='center' >
                     <Grid container size={{ xs: 12}} paddingInline={1.6} spacing={0} className='sponsor-form' sx={{ order: { xs: 1, lg: 2 } }}  >
