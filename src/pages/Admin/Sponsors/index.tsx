d6c8d65bc7bb2615b477b3c3eec624bd1b13b87c
Nevin <nevin@nivid.co>
2025-03-03 11:54:40 +0530

    Bug fixes

--- src/pages/Admin/Sponsors/index.tsx
+++ src/pages/Admin/Sponsors/index.tsx
@@ -7,7 +7,7 @@ import CustomButton from '@/components/CustomButton/CustomButton'
 import AddIcon from "@mui/icons-material/Add";
 import CustomDrawer from '@/components/CustomDrawer/CustomDrawer'
 import CloseIcon from "../../../assets/svg/Close.svg"
-import useStore, { POST, setNonPersistedDataById, snackBar } from '@/Libs/store'
+import useStore, { POST, setDataById, setNonPersistedDataById, snackBar } from '@/Libs/store'
 import CustomTextField from '@/components/CustomTextfield/CustomTextField'
 import FileUpload from '@/components/FileUpload/FileUpload'
 import { DataGridList } from '@/components/DataGrid/DataGridList'
@@ -342,6 +342,14 @@ const Sponsors = () => {
         } else {
             form.clearErrors('website');
         }
+        if(data?.logoId == ""){
+            setDataById('snackBarInfo', { open: true, autoHideDuration: 2000, severity: 'error', message: "Please upload Logo" })
+        return
+        }
+        if(data?.bannerId == ""){
+            setDataById('snackBarInfo', { open: true, autoHideDuration: 2000, severity: 'error', message: "Please upload Banner" })
+        return
+        }    
 
         const companyId = sessionStorage.getItem('companyId') || '';
 
